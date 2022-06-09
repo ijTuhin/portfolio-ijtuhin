9e86d327e127e03793cbd39a4296d538cd8fa615
ijTuhin <isratishu0209@gmail.com>
2022-06-09 22:07:15 +0600

    intro part sticked again

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Portfolio from './PortfolioPage/Portfolio/Portfolio';
 function App() {
   return (
-    <div className='mx-0 w-screen overflow-hidden'>
+    <div>
       <Portfolio></Portfolio>
     </div>
   );
