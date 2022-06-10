b994922f4ff3c6ea09cfa780a97bf5208f38a184
ijTuhin <isratishu0209@gmail.com>
2022-06-10 16:02:12 +0600

    flip on hover service section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Portfolio from './PortfolioPage/Portfolio/Portfolio';
+import fa from '../src/image/svg/plan-svgrepo-com.svg';
 function App() {
   return (
-    <div>
+    <div className='bg-white'>
       <Portfolio></Portfolio>
+      <img src={fa} className='bg-pink-300' alt="" />
     </div>
   );
 }
--- src/PortfolioPage/MyService/MyService.js
+++ src/PortfolioPage/MyService/MyService.js
@@ -3,16 +3,79 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import ScrollAnimation from 'react-animate-on-scroll';
 import 'animate.css';
+import './MyService.css';
 
 const MyService = () => {
     return (
         <div>
             <ScrollAnimation animateIn="animate__backInRight animate__slow" className='md:block hidden'>
-                <div className='flex flex-col justify-center items-center w-screen h-screen mb-96 md:mb-96'>
+                <div className='flex flex-col justify-center items-center w-screen h-screen mb-96 md:mb-6'>
 
                     <h1 className='text-gray-500 text-4xl font-medium mb-10'>My Services</h1>
                     <div className='grid grid-cols-1 gap-y-6 md:grid-cols-4 md:gap-x-6'>
-                        <div className='flex flex-col justify-center items-start w-[275px] h-72 pl-10 pr-3 shadow-2xl rounded-md bg-[#1a212b]/50 hover:shadow-md hover:text-amber-400/70'>
+
+                        <div class="flip-card">
+                            <div class="flip-card-inner">
+                                <div class="flip-card-front">
+                                    <div className='flex flex-col justify-center items-start w-[275px] h-72 pl-10 pr-3 shadow-2xl rounded-md bg-[#1a212b]/50 hover:shadow-md'>
+                                        <FontAwesomeIcon className='text-4xl' icon={faCode}></FontAwesomeIcon>
+                                        <h2 className='text-[1.5rem] font-semibold'>Web Development</h2>
+                                    </div>
+                                </div>
+                                <div class="flip-card-back">
+                                    <div className='flex flex-col justify-center items-start w-[275px] h-72 pl-10 pr-3 shadow-2xl rounded-md bg-[#1a212b]/50 hover:shadow-md text-white'>
+                                        <p>Lorem, ipsum dolor sit amet libero minima ex commodi! consectetur adipisicing elit. Odio eligendi itaque labore, expedita vel nam accusamus, consequuntur optio autem reiciendis provident soluta rem consectetur, ab repellendus </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flip-card">
+                            <div class="flip-card-inner">
+                                <div class="flip-card-front">
+                                    <div className='flex flex-col justify-center items-start w-[275px] h-72 pl-10 pr-3 shadow-2xl rounded-md bg-[#1a212b]/50 hover:shadow-md hover:text-amber-400/70'>
+                                        <FontAwesomeIcon className='text-4xl' icon={faTv}></FontAwesomeIcon>
+                                        <h2 className='text-[1.5rem] font-semibold'>UI/UX Design</h2>
+                                    </div>
+                                </div>
+                                <div class="flip-card-back">
+                                    <div className='flex flex-col justify-center items-start w-[275px] h-72 pl-10 pr-3 shadow-2xl rounded-md bg-[#1a212b]/50 hover:shadow-md text-white'>
+                                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio eligendi itaque labore, expedita vel nam accusamus, consequuntur optio autem reiciendis provident soluta rem consectetur, ab repellendus libero minima ex commodi!</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flip-card">
+                            <div class="flip-card-inner">
+                                <div class="flip-card-front">
+                                    <div className='flex flex-col justify-center items-start w-[275px] h-72 pl-10 pr-3 shadow-2xl rounded-md bg-[#1a212b]/50 hover:shadow-md hover:text-amber-400/70'>
+                                        <FontAwesomeIcon className='text-4xl' icon={faUserFriends}></FontAwesomeIcon>
+                                        <h2 className='text-[1.5rem] font-semibold'>Project Management</h2>
+                                    </div>
+                                </div>
+                                <div class="flip-card-back">
+                                    <div className='flex flex-col justify-center items-start w-[275px] h-72 pl-10 pr-3 shadow-2xl rounded-md bg-[#1a212b]/50 hover:shadow-md text-white'>
+                                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio eligendi itaque labore, expedita vel nam accusamus, consequuntur optio autem reiciendis provident soluta rem consectetur, ab repellendus libero minima ex commodi!</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flip-card">
+                            <div class="flip-card-inner">
+                                <div class="flip-card-front">
+                                    <div className='flex flex-col justify-center items-start w-[275px] h-72 pl-10 pr-3 shadow-2xl rounded-md bg-[#1a212b]/50 hover:shadow-md hover:text-amber-400/70'>
+                                        <FontAwesomeIcon className='text-4xl' icon={faCameraRetro}></FontAwesomeIcon>
+                                        <h2 className='text-[1.5rem] font-semibold'>Marketing & Strategy</h2>
+                                    </div>
+                                </div>
+                                <div class="flip-card-back">
+                                    <div className='flex flex-col justify-center items-start w-[275px] h-72 pl-10 pr-3 shadow-2xl rounded-md bg-[#1a212b]/50 hover:shadow-md text-white'>
+                                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio eligendi itaque labore, expedita vel nam accusamus, consequuntur optio autem reiciendis provident soluta rem consectetur, ab repellendus libero minima ex commodi!</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* <div className='flex flex-col justify-center items-start w-[275px] h-72 pl-10 pr-3 shadow-2xl rounded-md bg-[#1a212b]/50 hover:shadow-md hover:text-amber-400/70'>
                             <FontAwesomeIcon className='text-4xl' icon={faCode}></FontAwesomeIcon>
                             <h2 className='text-[1.5rem] font-semibold'>Web Development</h2>
                         </div>
@@ -27,7 +90,7 @@ const MyService = () => {
                         <div className='flex flex-col justify-center items-start w-[275px] h-72 pl-10 pr-3 shadow-2xl rounded-md bg-[#1a212b]/50 hover:shadow-md hover:text-amber-400/70'>
                             <FontAwesomeIcon className='text-4xl' icon={faCameraRetro}></FontAwesomeIcon>
                             <h2 className='text-[1.5rem] font-semibold'>Marketing & Strategy</h2>
-                        </div>
+                        </div> */}
                     </div>
 
                 </div>
@@ -37,27 +100,71 @@ const MyService = () => {
                 <h1 className='text-gray-500 text-4xl font-medium mb-10'>My Services</h1>
                 <div className='grid grid-cols-1 gap-y-6 md:grid-cols-4 md:gap-x-6'>
                     <ScrollAnimation animateIn="animate__backInRight animate__slow">
-                        <div className='flex flex-col justify-center items-start w-[275px] h-72 pl-10 pr-3 shadow-2xl rounded-md bg-[#1a212b]/50 hover:shadow-md hover:text-amber-400/70'>
-                            <FontAwesomeIcon className='text-4xl' icon={faCode}></FontAwesomeIcon>
-                            <h2 className='text-[1.5rem] font-semibold'>Web Development</h2>
+                        <div class="flip-card">
+                            <div class="flip-card-inner">
+                                <div class="flip-card-front">
+                                    <div className='flex flex-col justify-center items-start w-[275px] h-72 pl-10 pr-3 shadow-2xl rounded-md bg-[#1a212b]/50 hover:shadow-md'>
+                                        <FontAwesomeIcon className='text-4xl' icon={faCode}></FontAwesomeIcon>
+                                        <h2 className='text-[1.5rem] font-semibold'>Web Development</h2>
+                                    </div>
+                                </div>
+                                <div class="flip-card-back">
+                                    <div className='flex flex-col justify-center items-start w-[275px] h-72 pl-10 pr-3 shadow-2xl rounded-md bg-[#1a212b]/50 hover:shadow-md text-white'>
+                                        <p>Lorem, ipsum dolor sit amet libero minima ex commodi! consectetur adipisicing elit. Odio eligendi itaque labore, expedita vel nam accusamus, consequuntur optio autem reiciendis provident soluta rem consectetur, ab repellendus </p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </ScrollAnimation>
                     <ScrollAnimation animateIn="animate__backInLeft animate__slow">
-                        <div className='flex flex-col justify-center items-start w-[275px] h-72 pl-10 pr-3 shadow-2xl rounded-md bg-[#1a212b]/50 hover:shadow-md hover:text-amber-400/70'>
-                            <FontAwesomeIcon className='text-4xl' icon={faTv}></FontAwesomeIcon>
-                            <h2 className='text-[1.5rem] font-semibold'>UI/UX Design</h2>
+                        <div class="flip-card">
+                            <div class="flip-card-inner">
+                                <div class="flip-card-front">
+                                    <div className='flex flex-col justify-center items-start w-[275px] h-72 pl-10 pr-3 shadow-2xl rounded-md bg-[#1a212b]/50 hover:shadow-md hover:text-amber-400/70'>
+                                        <FontAwesomeIcon className='text-4xl' icon={faTv}></FontAwesomeIcon>
+                                        <h2 className='text-[1.5rem] font-semibold'>UI/UX Design</h2>
+                                    </div>
+                                </div>
+                                <div class="flip-card-back">
+                                    <div className='flex flex-col justify-center items-start w-[275px] h-72 pl-10 pr-3 shadow-2xl rounded-md bg-[#1a212b]/50 hover:shadow-md text-white'>
+                                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio eligendi itaque labore, expedita vel nam accusamus, consequuntur optio autem reiciendis provident soluta rem consectetur, ab repellendus libero minima ex commodi!</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </ScrollAnimation>
                     <ScrollAnimation animateIn="animate__backInRight animate__slow">
-                        <div className='flex flex-col justify-center items-start w-[275px] h-72 pl-10 pr-3 shadow-2xl rounded-md bg-[#1a212b]/50 hover:shadow-md hover:text-amber-400/70'>
-                            <FontAwesomeIcon className='text-4xl' icon={faUserFriends}></FontAwesomeIcon>
-                            <h2 className='text-[1.5rem] font-semibold'>Project Management</h2>
+                        <div class="flip-card">
+                            <div class="flip-card-inner">
+                                <div class="flip-card-front">
+                                    <div className='flex flex-col justify-center items-start w-[275px] h-72 pl-10 pr-3 shadow-2xl rounded-md bg-[#1a212b]/50 hover:shadow-md hover:text-amber-400/70'>
+                                        <FontAwesomeIcon className='text-4xl' icon={faUserFriends}></FontAwesomeIcon>
+                                        <h2 className='text-[1.5rem] font-semibold'>Project Management</h2>
+                                    </div>
+                                </div>
+                                <div class="flip-card-back">
+                                    <div className='flex flex-col justify-center items-start w-[275px] h-72 pl-10 pr-3 shadow-2xl rounded-md bg-[#1a212b]/50 hover:shadow-md text-white'>
+                                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio eligendi itaque labore, expedita vel nam accusamus, consequuntur optio autem reiciendis provident soluta rem consectetur, ab repellendus libero minima ex commodi!</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </ScrollAnimation>
                     <ScrollAnimation animateIn="animate__backInLeft animate__slow">
-                        <div className='flex flex-col justify-center items-start w-[275px] h-72 pl-10 pr-3 shadow-2xl rounded-md bg-[#1a212b]/50 hover:shadow-md hover:text-amber-400/70'>
-                            <FontAwesomeIcon className='text-4xl' icon={faCameraRetro}></FontAwesomeIcon>
-                            <h2 className='text-[1.5rem] font-semibold'>Marketing & Strategy</h2>
+                        <div class="flip-card">
+                            <div class="flip-card-inner">
+                                <div class="flip-card-front">
+                                    <div className='flex flex-col justify-center items-start w-[275px] h-72 pl-10 pr-3 shadow-2xl rounded-md bg-[#1a212b]/50 hover:shadow-md hover:text-amber-400/70'>
+                                        <FontAwesomeIcon className='text-4xl' icon={faCameraRetro}></FontAwesomeIcon>
+                                        <h2 className='text-[1.5rem] font-semibold'>Marketing & Strategy</h2>
+                                    </div>
+                                </div>
+                                <div class="flip-card-back">
+                                    <div className='flex flex-col justify-center items-start w-[275px] h-72 pl-10 pr-3 shadow-2xl rounded-md bg-[#1a212b]/50 hover:shadow-md text-white'>
+                                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio eligendi itaque labore, expedita vel nam accusamus, consequuntur optio autem reiciendis provident soluta rem consectetur, ab repellendus libero minima ex commodi!</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </ScrollAnimation>
                 </div>
