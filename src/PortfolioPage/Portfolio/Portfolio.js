b39c2df22319ec64411817a8379ad299fe1dd968
ijTuhin <isratishu0209@gmail.com>
2022-06-10 22:25:57 +0600

    intro part bg-color changed

--- src/PortfolioPage/Portfolio/Portfolio.js
+++ src/PortfolioPage/Portfolio/Portfolio.js
@@ -8,11 +8,10 @@ import MySkills from '../MySkills/MySkills';
 import PortfolioNavbar from '../PorfolioNavBar/PorfolioNavBar';
 import ProblemSolving from '../ProblemSolving/ProblemSolving';
 import Projects from '../Projects/Projects';
-import fa from '../../image/vector/cjrhaw9w300p3gvg0dmeg50vn-invention-email.one-half.png';
 
 const Portfolio = () => {
     return (
-        <div className='absolute bg-gray-900 text-gray-500'>
+        <div className='absolute bg-gradient-to-r from-[#232121] to-gray-900 text-gray-500'>
             <MainBodyMenu></MainBodyMenu>
             <IntroPart></IntroPart>
             <PortfolioNavbar></PortfolioNavbar>
@@ -32,10 +31,6 @@ const Portfolio = () => {
                 </div>
                 <div id='contact'>
                     <ContactMe></ContactMe>
-                    {/* <img className='w-96' src={fa} alt="" />
-                    <div className='bg-yellow-200'>
-
-                    </div> */}
                 </div>
             </div>
         </div>
