42a30029878bab20c0dfc8ee7c25dce208f53ac5
ijTuhin <isratishu0209@gmail.com>
2022-06-10 12:00:29 +0600

    mobile responsive animation for service section

--- src/PortfolioPage/MyService/MyService.js
+++ src/PortfolioPage/MyService/MyService.js
@@ -32,6 +32,37 @@ const MyService = () => {
 
                 </div>
             </ScrollAnimation>
+            <div className='flex flex-col justify-center items-center w-screen h-screen mb-96 md:mb-96 md:hidden'>
+
+                <h1 className='text-gray-500 text-4xl font-medium mb-10'>My Services</h1>
+                <div className='grid grid-cols-1 gap-y-6 md:grid-cols-4 md:gap-x-6'>
+                    <ScrollAnimation animateIn="animate__backInRight animate__slow">
+                        <div className='flex flex-col justify-center items-start w-[275px] h-72 pl-10 pr-3 shadow-2xl rounded-md bg-[#1a212b]/50 hover:shadow-md hover:text-amber-400/70'>
+                            <FontAwesomeIcon className='text-4xl' icon={faCode}></FontAwesomeIcon>
+                            <h2 className='text-[1.5rem] font-semibold'>Web Development</h2>
+                        </div>
+                    </ScrollAnimation>
+                    <ScrollAnimation animateIn="animate__backInLeft animate__slow">
+                        <div className='flex flex-col justify-center items-start w-[275px] h-72 pl-10 pr-3 shadow-2xl rounded-md bg-[#1a212b]/50 hover:shadow-md hover:text-amber-400/70'>
+                            <FontAwesomeIcon className='text-4xl' icon={faTv}></FontAwesomeIcon>
+                            <h2 className='text-[1.5rem] font-semibold'>UI/UX Design</h2>
+                        </div>
+                    </ScrollAnimation>
+                    <ScrollAnimation animateIn="animate__backInRight animate__slow">
+                        <div className='flex flex-col justify-center items-start w-[275px] h-72 pl-10 pr-3 shadow-2xl rounded-md bg-[#1a212b]/50 hover:shadow-md hover:text-amber-400/70'>
+                            <FontAwesomeIcon className='text-4xl' icon={faUserFriends}></FontAwesomeIcon>
+                            <h2 className='text-[1.5rem] font-semibold'>Project Management</h2>
+                        </div>
+                    </ScrollAnimation>
+                    <ScrollAnimation animateIn="animate__backInLeft animate__slow">
+                        <div className='flex flex-col justify-center items-start w-[275px] h-72 pl-10 pr-3 shadow-2xl rounded-md bg-[#1a212b]/50 hover:shadow-md hover:text-amber-400/70'>
+                            <FontAwesomeIcon className='text-4xl' icon={faCameraRetro}></FontAwesomeIcon>
+                            <h2 className='text-[1.5rem] font-semibold'>Marketing & Strategy</h2>
+                        </div>
+                    </ScrollAnimation>
+                </div>
+
+            </div>
         </div>
     );
 };
