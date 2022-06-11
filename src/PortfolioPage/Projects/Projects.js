4dcc5dc34dbaca38a112ea032fbdb25e541a0ab7
ijTuhin <isratishu0209@gmail.com>
2022-06-11 06:17:31 +0600

    individual project route done

--- src/PortfolioPage/Projects/Projects.js
+++ src/PortfolioPage/Projects/Projects.js
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { projectI, projectII, projectIII, projectIV, projectV } from '../../index';
 import './Projects.css';
