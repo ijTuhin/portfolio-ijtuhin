0a893b1652fe53f66b0cdce5a438468e3506dee5
ijTuhin <isratishu0209@gmail.com>
2022-06-11 21:09:30 +0600

    error message for contact form

--- src/PortfolioPage/ContactMe/ContactMe.js
+++ src/PortfolioPage/ContactMe/ContactMe.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 const ContactMe = () => {
     const { register, handleSubmit } = useForm();
+    const [submit, setSubmit] = useState(false);
+    const [display, setDisplay] = useState(false);
     const onSubmit = data => {
-        console.log(data);
-        data = '';
+        setSubmit(true);
+        if (data) {
+            setDisplay(true);
+        }
+        else {
+            setDisplay(false);
+        }
     };
 
     return (
@@ -17,13 +24,26 @@ const ContactMe = () => {
                         <p>Got a question or proposal, or just want
                             to say hello? Go ahead.</p>
                     </div>
+                    {
+                        submit ?
+                            <>
+                                {
+                                    display ?
+                                        <><p className='text-cyan-500 font-mono z-50'>Thanks for your interest!!!</p></>
+                                        :
+                                        <><p className='text-red-500 font-mono'>Form is empty!!!</p></>
+                                }
+                            </>
+                            :
+                            <></>
+                    }
                     <div className='w-[600px] mt-8'>
                         <div className='grid grid-cols-2 mb-6'>
-                            <input placeholder='Your Name' className='form-control bg-transparent placeholder-gray-600 text-gray-200 border-b border-gray-300 p-2 w-[275px]' {...register("name")} />
-                            <input placeholder='Email Address' className='form-control bg-transparent placeholder-gray-600 text-gray-200 border-b border-gray-300 p-2 w-[275px]' {...register("email")} /> <br />
+                            <input placeholder='Your Name' className='form-control bg-transparent placeholder-gray-600 text-gray-200 border-b border-gray-300 p-2 w-[275px]' {...register("name", { required: true })} />
+                            <input placeholder='Email Address' className='form-control bg-transparent placeholder-gray-600 text-gray-200 border-b border-gray-300 p-2 w-[275px]' {...register("email", { required: true })} /> <br />
                         </div>
                     </div>
-                    <textarea className="form-control bg-transparent placeholder-gray-600 text-gray-200 border-b border-gray-300 mb-2 p-2 w-[580px]" placeholder='Hi, we have a project "X" at our company "Y". How soon can you hop on to discuss this? ' rows="2" {...register("feedback")} />
+                    <textarea className="form-control bg-transparent placeholder-gray-600 text-gray-200 border-b border-gray-300 mb-2 p-2 w-[580px]" placeholder='Hi, we have a project "X" at our company "Y". How soon can you hop on to discuss this? ' rows="2" {...register("feedback", { required: true })} />
                     <div className='flex justify-end mr-5'>
                         <button className='bg-gray-500 text-gray-800 hover:bg-cyan-600 hover:text-black px-4 py-1.5 rounded'>Submit</button>
                     </div>
